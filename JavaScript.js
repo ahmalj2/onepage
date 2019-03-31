615991f4532674f1735d7a8eb6d1bf02f53c508e
ahmalj2 <43953864+ahmalj2@users.noreply.github.com>
2019-03-31 21:24:30 +0200

    onepage

---
+++ JavaScript.js
@@ -0,0 +1,30 @@
+
+$('header').show();
+$('map').show(); 
+
+
+//moving to the targeted element smoothely in 2 s
+$("#home").click(function() {
+    $('html,body').animate({
+      scrollTop:$("#second-footer").offset().top}, 2000);
+    });
+
+    $("#home").click(function() {
+          $('html,body').animate({
+            scrollTop:$("#second-footer").offset().top}, 2000);
+          });
+
+          $("#home").click(function() {
+                $('html,body').animate({
+                  scrollTop:$("#second-footer").offset().top}, 2000);
+                });
+
+                $("#home").click(function() {
+                      $('html,body').animate({
+                        scrollTop:$("#second-footer").offset().top}, 2000);
+                      });
+
+                      $("#home").click(function() {
+                            $('html,body').animate({
+                              scrollTop:$("#second-footer").offset().top}, 2000);
+                            });
