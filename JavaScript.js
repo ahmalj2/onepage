89847f02ae2e050e5979e84c3b1d52793a042d5a
ahmalj2 <43953864+ahmalj2@users.noreply.github.com>
2019-04-01 21:41:03 +0200

    best version

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,5 @@
-
 $('header').show();
-$('map').show(); 
+$('map').show();
 
 
 //moving to the targeted element smoothely in 2 s
@@ -9,22 +8,23 @@ $("#home").click(function() {
       scrollTop:$("#second-footer").offset().top}, 2000);
     });
 
-    $("#home").click(function() {
-          $('html,body').animate({
-            scrollTop:$("#second-footer").offset().top}, 2000);
-          });
+$("#coaches").click(function() {
+      $('html,body').animate({
+        scrollTop:$(".couches").offset().top}, 2000);
+      });
 
-          $("#home").click(function() {
-                $('html,body').animate({
-                  scrollTop:$("#second-footer").offset().top}, 2000);
-                });
+$("#priser").click(function() {
+      $('html,body').animate({
+        scrollTop:$(".priser").offset().top}, 2000);
+      });
 
-                $("#home").click(function() {
-                      $('html,body').animate({
-                        scrollTop:$("#second-footer").offset().top}, 2000);
-                      });
+$("#kontakt").click(function() {
+      $('html,body').animate({
+        scrollTop:$(".container").offset().top}, 2000);
+      });
 
-                      $("#home").click(function() {
-                            $('html,body').animate({
-                              scrollTop:$("#second-footer").offset().top}, 2000);
-                            });
+
+$(".readmore").click(function() {
+      $('html,body').animate({
+        scrollTop:$(".target").offset().top}, 2000);
+      });
